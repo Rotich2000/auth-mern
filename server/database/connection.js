7bd576ffccb83f5f62c188276667290a97a09698
Rotich2000 <drkimutai06@gmail.com>
2023-03-29 19:09:51 +0300

    secured mondb uri

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async() => {
     try {
-        const connect = await mongoose.connect('mongodb+srv://kim:kim@cluster0.mj1it.mongodb.net/mern-stack-auth?retryWrites=true&w=majority');
+        const connect = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`MongoDB Connected: ${connect.connection.host}`);
     } catch (error) {
         console.log(`MongoDB Connection failed ${error}`)
